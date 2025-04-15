494582343f96deae81837664e12e36415fd7a69e
johnkenzzy <johnkenumeh@gmail.com>
2025-04-15 16:48:12 +0100

    Lint fix

--- 0x00-ES6_basic/1-block-scoped.js
+++ 0x00-ES6_basic/1-block-scoped.js
@@ -3,9 +3,8 @@ export default function taskBlock(trueOrFalse) {
   const task2 = true;
 
   if (trueOrFalse) {
-    let task = true;
+    const task = true;
     const task2 = !task;
-    task = !task2;
   }
 
   return [task, task2];
