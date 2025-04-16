b47717f490726f3bb5b8d7ada83e1d4f4141b6dd
johnkenzzy <johnkenumeh@gmail.com>
2025-04-16 17:04:53 +0100

    Complete task 3

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,11 @@
+import { uploadPhoto, createUser } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([uploadPhoto(), createUser()])
+    .then(([p, u]) => {
+      console.log(`${p.body} ${u.firstName} ${u.lastName}`);
+    })
+    .catch(() => {
+      console.log('Signup system offline');
+    });
+}
