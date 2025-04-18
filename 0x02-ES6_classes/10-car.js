5ba62bfe1d5020bd0b16c47ca0ceae962b81b256
johnkenzzy <johnkenumeh@gmail.com>
2025-04-18 04:39:50 +0100

    Complete task10

---
+++ 0x02-ES6_classes/10-car.js
@@ -0,0 +1,17 @@
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  // Symbol.species getter - allows overriding
+  static get [Symbol.species]() {
+    return this;
+  }
+
+  cloneCar() {
+    const Cls = this.constructor[Symbol.species];
+    return new Cls(this._brand, this._motor, this._color);
+  }
+}
