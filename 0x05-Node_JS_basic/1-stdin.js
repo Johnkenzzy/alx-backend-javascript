086169ebeac7e1045c0bc90183b12dbaeb332b9b
Johnkenzzy <johnkenumeh@gmail.com>
2025-06-10 19:13:11 +0100

    Fix to meet requirements

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,12 @@
 process.stdout.write('Welcome to ALX, what is your name?\n');
 
-process.stdin.on('readable', () => {
-  const input = process.stdin.read();
-  if (input !== null) {
-    process.stdout.write(`Your name is: ${input}`);
-  }
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('data', (input) => {
+  const name = input.trim();
+  console.log(`Your name is: ${name}`);
 });
 
 process.stdin.on('end', () => {
-  process.stdout.write('This important software is now closing\n');
+  console.log('This important software is now closing');
 });
