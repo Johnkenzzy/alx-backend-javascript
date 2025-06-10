5d5906774503c2f7bc0a4e6334c0db2c33292a0e
Johnkenzzy <johnkenumeh@gmail.com>
2025-06-10 19:03:44 +0100

    Remove explicit utf8 encoding

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,12 +1,9 @@
-process.stdin.setEncoding('utf8');
-
 process.stdout.write('Welcome to ALX, what is your name?\n');
 
 process.stdin.on('readable', () => {
   const input = process.stdin.read();
   if (input !== null) {
-    const trimmedInput = input.trim();
-    process.stdout.write(`Your name is: ${trimmedInput}\n`);
+    process.stdout.write(`Your name is: ${input}`);
   }
 });
 
