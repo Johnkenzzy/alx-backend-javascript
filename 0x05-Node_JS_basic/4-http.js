c6b3a08b270ae238d95e05697b7126391a69d4a0
Johnkenzzy <johnkenumeh@gmail.com>
2025-06-11 04:20:12 +0100

    Create a small HTTP server using Node's HTTP module

---
+++ 0x05-Node_JS_basic/4-http.js
@@ -0,0 +1,11 @@
+const {createServer} = require('http');
+
+const app = createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello ALX!');
+});
+
+app.listen(1245, 'localhost');
+
+module.exports = app;
