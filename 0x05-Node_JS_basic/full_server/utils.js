96d7f0a98f2cffcd9f6343d4bc649ccbbb874336
Johnkenzzy <johnkenumeh@gmail.com>
2025-06-11 05:59:47 +0100

    Organize a complex HTTP server using Express

---
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -0,0 +1,24 @@
+import fs from 'fs';
+
+export function readDatabase(filePath) {
+  return new Promise((resolve, reject) => {
+    fs.readFile(filePath, 'utf8', (err, data) => {
+      if (err) {
+        reject(Error('Cannot load the database'));
+        return;
+      }
+
+      const lines = data.split('\n').filter((line) => line.trim() !== '');
+      const students = lines.slice(1); // skip header
+      const fields = {};
+
+      for (const line of students) {
+        const [firstname, , , field] = line.split(',');
+        if (!fields[field]) fields[field] = [];
+        fields[field].push(firstname);
+      }
+
+      resolve(fields);
+    });
+  });
+}
