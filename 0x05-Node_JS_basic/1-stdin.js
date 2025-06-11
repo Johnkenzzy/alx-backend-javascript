8c1a8cc4373794d000515c2025241ddd2d361e37
Johnkenzzy <johnkenumeh@gmail.com>
2025-06-11 02:35:59 +0100

    Revert code and change to 'Holberton School'

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -1,10 +1,12 @@
-process.stdout.write('Welcome to ALX, what is your name?\n');
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
 process.stdin.setEncoding('utf8');
 
-process.stdin.on('data', (input) => {
-  const name = input.trim();
-  console.log(`Your name is: ${name}`);
+process.stdin.on('readable', () => {
+  const input = process.stdin.read();
+  if (input) {
+    process.stdout.write(`Your name is: ${name}`);
+  }
 });
 
 process.stdin.on('end', () => {
